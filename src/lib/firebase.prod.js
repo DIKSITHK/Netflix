aec8697759403bbd29d5232763ba3d22122ad547
DIKSITHK <71424829+DIKSITHK@users.noreply.github.com>
2021-07-20 16:02:55 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,13 +5,13 @@ import 'firebase/auth';
 
 
 const config = {
-    apiKey: "AIzaSyBrxV-mEH5RMjh1o3Q07fMfFr-10bz3iUM",
-    authDomain: "netflix-clone-49789.firebaseapp.com",
-    projectId: "netflix-clone-49789",
-    storageBucket: "netflix-clone-49789.appspot.com",
-    messagingSenderId: "760313783509",
-    appId: "1:760313783509:web:b17958885cbfd5e700ae5c",
-    measurementId: "G-05CVLMRCZ4"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
